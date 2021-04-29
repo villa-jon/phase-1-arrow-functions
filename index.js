69c6045973cb42278eb9e68a3cc06859370e98e9
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-04-29 01:40:24 -0700

    commit tree e2ba022bed49789c8c9b2b4d277edba86da376dc

---
+++ index.js
@@ -0,0 +1,19 @@
+const divide = () => {
+    let one = 2000
+    let two = 100
+    let result = one / two
+    return result 
+}
+
+const square = () => {
+    let x = 2
+    let result = x*x 
+    return result 
+}
+
+const add = () => {
+    let a = 3
+    let b = 4
+    let result = a + b
+    return result 
+}
